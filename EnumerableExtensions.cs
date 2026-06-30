7c87f138d093946d003a2803b70911a71d24ecec
Pete Schmitz <carbonsoft707@gmail.com>
2026-06-30 01:20:36 -0700

    add ismatchingstringarrays helper

--- EnumerableExtensions.cs
+++ EnumerableExtensions.cs
@@ -6,4 +6,21 @@ public static class EnumerableExtensions
     {
         return sequence ?? [];
     }
+
+    public static bool IsMatchingStringArrays(this string[]? array1, string[]? array2)
+    {
+        if (array1 == null && array2 == null)
+        {
+            return true;
+        }
+        if (array1 == null || array2 == null)
+        {
+            return false;
+        }
+        if (array1.Length != array2.Length)
+        {
+            return false;
+        }
+        return array1.SequenceEqual(array2);
+    }
 }
